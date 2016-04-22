b609dd9d5a7f08d1a70bcbbfe9b2d1d5d3ec3d13
Vojtech Szocs <vszocs@redhat.com>
2016-04-22 18:38:28 +0200

    automation: Lint code and run tests on patch validation
    CI scripts 'check-patch' and 'check-merged', executed
    by oVirt Jenkins to validate Gerrit patches, now lint
    code and run tests (as they should).
    
    For details, refer to oVirt build and test standards:
    
    http://www.ovirt.org/develop/dev-process/build-and-test-standards/
    
    Change-Id: I453bf0bbd324f502e65155901bb638472aa3b49b
    Signed-off-by: Vojtech Szocs <vszocs@redhat.com>

--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -ex
 
-# Nothing yet.
+# Set up Node.js environment with npm dependencies linked to ./node_modules:
+source /usr/share/ovirt-engine-nodejs-modules/setup-env.sh
+
+# Lint code and run tests:
+npm test
