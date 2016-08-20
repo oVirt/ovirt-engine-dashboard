oVirt/ovirt-engine-dashboard

commit 6a147e8f231f79af2b2ea5748164afa3f3467c9f

dashboard: update UtilizationBarChart to ellipsize name
When the name is truncated, show an ellipsis at the end of the
name.

Change-Id: Iec88d23ec0a76b9ca0c25f02d56b15ed589d83af
Bug-Url: https://bugzilla.redhat.com/1345840
Signed-off-by: Greg Sheremeta <gshereme@redhat.com>

src/components/ObjectUtilizationList.jsx
@@ -3,6 +3,11 @@ const { string, number, shape, oneOf, arrayOf, func } = PropTypes
 import classNames from 'classnames'
 import UtilizationBarChart from './patternfly/UtilizationBarChart'
 
+/*
+  TODO(gs)
+  swap out col-md-2/9 for 3/8 when any name is longer than X chars
+ */
+
 function ObjectUtilizationList ({ data, unit, emptyListText, thresholds, utilizationFooterLabel, onObjectNameClick }) {
   if (data.length === 0) {
     return (
@@ -19,13 +24,13 @@ function ObjectUtilizationList ({ data, unit, emptyListText, thresholds, utiliza
       <div className='overutilized-section'>
         {sortedData.map((item) => (
           <div key={item.name} className='row'>
-            <div className='col-md-2 text-right'>
-              <a href='#' onClick={(event) => {
+            <div className='col-md-2 text-right overutilized-item-name-container'>
+              <a className='overutilized-item-name' href='#' onClick={(event) => {
                 event.preventDefault()
                 onObjectNameClick(item)
               }}>{item.name}</a>
             </div>
-            <div className='col-md-9'>
+            <div className='col-md-9 overutilized-item-bar'>
               <UtilizationBarChart
                 used={item.used}
                 total={item.total}
@@ -34,7 +39,7 @@ function ObjectUtilizationList ({ data, unit, emptyListText, thresholds, utiliza
                 layout='inline'
                 footerLabel={utilizationFooterLabel} />
             </div>
-            <div className='col-md-1'>
+            <div className='col-md-1 overutilized-item-trend'>
               {item.trend !== 'same' &&
                 <span className={classNames('pficon', {
                   'pficon-trend-up': item.trend === 'up',

static/css/angular-app.css
@@ -383,18 +383,24 @@
 .overutilization-dialog .overutilized-section .progress-container.progress-description-left {
   padding-left: 0px;
 }
+.overutilization-dialog .overutilized-section .overutilized-item-name {
+  display: block;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
 .overutilization-dialog .overutilized-section .progress-label-right .progress-bar span {
   margin-right: 20px;
 }
-.overutilization-dialog .overutilized-section .col-md-2.text-right {
+.overutilization-dialog .overutilized-section .overutilized-item-name-container {
+  padding-right: 0px;
+}
+.overutilization-dialog .overutilized-section .overutilized-item-trend {
   padding-left: 0px;
   padding-right: 0px;
 }
-.overutilization-dialog .overutilized-section .col-md-1 {
-  padding-left: 0px;
-  padding-right: 0px;
-}
-.overutilization-dialog .overutilized-section .col-md-9 {
+.overutilization-dialog .overutilized-section .overutilized-item-bar {
   padding-right: 0px;
 }
 .d3-context-menu {

commit 748b2f5d4b4ab8e483f759d96a54b3b1879c8089

fix ObjectUtilizationList alignment
commit 6a147e8 introduced a bug where the bar charts would
stagger (names and bars no longer lining up) when there were
both short and long names. Fixed.

Change-Id: If8eac712ac066994a745acdad38d87ff40939dbb
Bug-Url: https://bugzilla.redhat.com/1345840
Signed-off-by: Greg Sheremeta <gshereme@redhat.com>

src/components/ObjectUtilizationList.jsx
@@ -11,30 +11,36 @@ function ObjectUtilizationList ({ data, unit, emptyListText, thresholds, utiliza
     )
   }
 
+  function namePastThreshold (sortedData) {
+    for (let item of sortedData) {
+      if (item.name.length > utilizationListGridNameThreshold) {
+        return true
+      }
+    }
+    return false
+  }
+
   const sortedData = data.slice().sort((a, b) => {
     return (b.used / b.total) - (a.used / a.total)
   })
 
-  const nameThresholdClass = (item) => (
-    item.name.length <= utilizationListGridNameThreshold ? 'col-md-2' : 'col-md-3'
-  )
+  let hasNamePastThreshold = namePastThreshold(sortedData)
 
-  const barThresholdClass = (item) => (
-    item.name.length <= utilizationListGridNameThreshold ? 'col-md-9' : 'col-md-8'
-  )
+  const nameThresholdClass = hasNamePastThreshold ? 'col-md-3' : 'col-md-2'
+  const barThresholdClass = hasNamePastThreshold ? 'col-md-8' : 'col-md-9'
 
   return (
     <div className='overutilized-container'>
       <div className='overutilized-section'>
         {sortedData.map((item) => (
           <div key={item.name} className='row'>
-            <div className={`text-right overutilized-item-name-container ${nameThresholdClass(item)}`}>
+            <div className={`text-right overutilized-item-name-container ${nameThresholdClass}`}>
               <a className='overutilized-item-name' href='#' onClick={(event) => {
                 event.preventDefault()
                 onObjectNameClick(item)
               }}>{item.name}</a>
             </div>
-            <div className={`overutilized-item-bar ${barThresholdClass(item)}`}>
+            <div className={`overutilized-item-bar ${barThresholdClass}`}>
               <UtilizationBarChart
                 used={item.used}
                 total={item.total}