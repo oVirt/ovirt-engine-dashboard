b8019ec032b1a580f22f6cca016e57fe31e29245
Greg Sheremeta <gshereme@redhat.com>
2017-02-03 15:13:49 -0500

    using yarn for build
    Switched to use yarn for build and dependency management.
    
    Also needed to add 'fontconfig' package for a phantomjs update.
    
    Change-Id: I9fe8565b192fc5cd50851c55f4e52051a21e1594
    Signed-off-by: Greg Sheremeta <gshereme@redhat.com>
    Signed-off-by: Vojtech Szocs <vszocs@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,7 +1,1 @@
-#!/bin/sh -ex
-
-# Set up Node.js environment with npm dependencies linked to ./node_modules:
-source /usr/share/ovirt-engine-nodejs-modules/setup-env.sh
-
-# Lint code and run tests:
-npm test
+check.sh
