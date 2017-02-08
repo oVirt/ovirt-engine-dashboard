5d567f5c2bbc1b932dfa7f598419deb7d5fa90c2
Vojtech Szocs <vszocs@redhat.com>
2017-02-08 16:00:41 +0100

    Use "yarn check" in check & build scripts
    This ensures consistency between package.json and yarn.lock files.
    
    Change-Id: Ife7c2ae2fb843d3b1b515bc31b546fdf20c12ac0
    Signed-off-by: Vojtech Szocs <vszocs@redhat.com>

--- automation/check.sh
+++ automation/check.sh
@@ -8,5 +8,9 @@ yum-deprecated -y install ${dependencies} || yum -y install ${dependencies}
 # Set up Node.js environment with dependencies linked to ./node_modules:
 source /usr/share/ovirt-engine-nodejs-modules/setup-env.sh
 
+# Verify that dependency versions (semver ranges) in package.json have
+# matching resolutions in the yarn.lock file:
+yarn check
+
 # Lint code and run tests:
 yarn test
